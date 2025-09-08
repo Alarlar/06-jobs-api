16e95f5ac9ffe0c77d695fb3cad58b30fe1a5a31
Alarlar <52812499+Alarlar@users.noreply.github.com>
2025-09-07 21:26:39 -0700

    register user, validate,hash password,save user

---
+++ controllers/collections.js
@@ -0,0 +1,23 @@
+const getAllCollections = async (req, res) => {
+  res.send("get all collections");
+};
+const getCollection = async (req, res) => {
+  res.send("get collection");
+};
+const createCollection = async (req, res) => {
+  res.send("create a collection");
+};
+const updateCollection = async (req, res) => {
+  res.send("update collection");
+};
+const deleteCollection = async (req, res) => {
+  res.send("delete collection");
+};
+
+module.exports = {
+  getAllCollections,
+  getCollection,
+  createCollection,
+  updateCollection,
+  deleteCollection,
+};
