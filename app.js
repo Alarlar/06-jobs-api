43cf0b6374fcd7e1292b561e7bf4f954a9c30f57
Alarlar <52812499+Alarlar@users.noreply.github.com>
2025-10-19 21:03:09 -0700

    jobs controllers are done, Mongoose Errors

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ require("express-async-errors");
 const express = require("express");
 const app = express();
 
+// extra sec pack
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss");
+const rateLimiter = require("express-rate-limit");
+const mongoSanitize = require("express-mongo-sanitize");
+const hpp = require("hpp");
+
 // connectDB
 const connectDB = require("./db/connect");
 const authenticateUser = require("./middleware/authentication");
@@ -14,8 +22,18 @@ const jobRouter = require("./routes/jobs");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15min
+    max: 100, // ip to 100 requests per wind
+  })
+);
 app.use(express.json());
-// extra packages
+app.use(helmet());
+app.use(cors());
+app.use(mongoSanitize());
+app.use(hpp());
 
 // routes
 app.use("/api/v1/auth", authRouter);
